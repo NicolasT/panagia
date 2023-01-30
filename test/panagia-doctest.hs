8d6aeef3658bf59bd80437e5f60ab68cad8cb429
Nicolas Trangez <ikke@nicolast.be>
2023-01-30 16:23:41 +0100

    tests: use `cabal-doctest` to run `doctest`
    Sadly enough, integrating `doctest` in a Cabal-based project, running
    the test as a `test-suite`, is not trivial: a whole load of arguments
    need to be passed to `doctest` for it to find all dependencies, and
    one needs to ensure `QuickCheck` and `template-haskell` are available
    when using the `prop>` feature, including `import`in `Test.QuickCheck`
    in a module under test (e.g., in a `$setup` section).
    
    This patch migrates the previous approach, which worked but doesn't
    scale once we add more features/dependencies, to use `cabal-doctest`
    which relies on using the `Custom` `build-type` to generate some module
    which can then be consumed from a test `Main` module.
    
    Despite this project currently having only a single exposed artifact
    (i.e., the `panagia` library), the changes made are as-if it's a
    multi-component project already, to account for future expansion.
    
    These changes make building all Haddocks somehow fail, hence we now only
    build the ones for libraries in CI.
    
    See: https://hackage.haskell.org/package/cabal-doctest
    See: https://github.com/haskellari/cabal-doctest
    See: https://github.com/haskellari/cabal-doctest/tree/44f04e6d7563066e3373675a834ea3bf9c095bfb/multiple-components-example

--- test/panagia-doctest.hs
+++ test/panagia-doctest.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main (main) where
 
+import Build_doctests (Component (..), Name (..), components)
+import Data.Foldable (for_)
+import Data.Maybe (fromMaybe)
+import System.Environment (unsetEnv)
 import Test.DocTest (doctest)
 
 main :: IO ()
-main = doctest ["src/"]
+main = for_ components $ \(Component name flags pkgs sources) -> do
+  putStrLn $ "Running doctests for " <> showName name
+  let args = flags ++ pkgs ++ sources
+  unsetEnv "GHC_ENVIRONMENT"
+  doctest args
+  where
+    showName = \case
+      NameLib l -> "lib:" <> fromMaybe "panagia" l
+      NameExe e -> "exe:" <> e
